36f61a8c78907019a4ce26ed37d2deabbb5cf862
Ray Smets <rayjsmets@gmail.com>
2021-01-20 08:55:40 -0800

    [Tests] checkCrenentialStatus.

--- test/checkCredentialStatus.test.ts
+++ test/checkCredentialStatus.test.ts
@@ -11,49 +11,43 @@ jest.mock('library-issuer-verifier-utility', () => ({
 
 const mockMakeNetworkRequest = makeNetworkRequest as jest.Mock;
 
-describe('fakeTest', () => {
-  it('passes a trivial test', () => {
-    expect(true).toEqual(true);
+describe('checkCredentialStatus', () => {
+  let credential1;
+  let credential2;
+  const authHeader = `Bearer ${dummyAuthToken}`;
+
+  beforeAll(async () => {
+    const unsignedCredential1 = makeDummyUnsignedCredential();
+    const unsignedCredential2 = makeDummyUnsignedCredential();
+    credential1 = await makeDummyCredential({ unsignedCredential: unsignedCredential1 });
+    credential2 = await makeDummyCredential({ unsignedCredential: unsignedCredential2 });
+  });
+
+  afterEach(() => {
+    mockMakeNetworkRequest.mockClear();
+  });
+
+  it('calls the SaaS api to check the credential\'s status', async () => {
+    mockMakeNetworkRequest.mockResolvedValueOnce({ body: { status: 'valid' } });
+
+    await checkCredentialStatus(credential1, authHeader);
+    expect(mockMakeNetworkRequest).toBeCalled();
+
+    const receivedArgs = mockMakeNetworkRequest.mock.calls[0][0];
+    expect(receivedArgs.baseUrl).toEqual(configData.SaaSUrl);
+    expect(receivedArgs.endPoint).toEqual(`credentialStatus/${credential1.id}`);
+  });
+
+  it('returns true if the credential status is valid', async () => {
+    mockMakeNetworkRequest.mockResolvedValueOnce({ body: { status: 'valid' } });
+    const isValid = await checkCredentialStatus(credential1, authHeader);
+    expect(isValid).toBe(true);
+  });
+
+  it('returns false if the credential status is revoked', async () => {
+    mockMakeNetworkRequest.mockResolvedValueOnce({ body: { status: 'revoked' } });
+
+    const isValid = await checkCredentialStatus(credential2, authHeader);
+    expect(isValid).toBe(false);
   });
 });
-
-// describe('checkCredentialStatus', () => {
-//   let credential1;
-//   let credential2;
-//   const authHeader = `Bearer ${dummyAuthToken}`;
-
-//   beforeAll(async () => {
-//     const unsignedCredential1 = makeDummyUnsignedCredential();
-//     const unsignedCredential2 = makeDummyUnsignedCredential();
-//     credential1 = await makeDummyCredential({ unsignedCredential: unsignedCredential1 });
-//     credential2 = await makeDummyCredential({ unsignedCredential: unsignedCredential2 });
-//   });
-
-//   afterEach(() => {
-//     mockMakeNetworkRequest.mockClear();
-//   });
-
-//   it('calls the SaaS api to check the credential\'s status', async () => {
-//     mockMakeNetworkRequest.mockResolvedValueOnce({ body: { status: 'valid' } });
-
-//     await checkCredentialStatus(credential1, authHeader);
-//     expect(mockMakeNetworkRequest).toBeCalled();
-
-//     const receivedArgs = mockMakeNetworkRequest.mock.calls[0][0];
-//     expect(receivedArgs.baseUrl).toEqual(configData.SaaSUrl);
-//     expect(receivedArgs.endPoint).toEqual(`credentialStatus/${credential1.id}`);
-//   });
-
-//   it('returns true if the credential status is valid', async () => {
-//     mockMakeNetworkRequest.mockResolvedValueOnce({ body: { status: 'valid' } });
-//     const isValid = await checkCredentialStatus(credential1, authHeader);
-//     expect(isValid).toBe(true);
-//   });
-
-//   it('returns false if the credential status is revoked', async () => {
-//     mockMakeNetworkRequest.mockResolvedValueOnce({ body: { status: 'revoked' } });
-
-//     const isValid = await checkCredentialStatus(credential2, authHeader);
-//     expect(isValid).toBe(false);
-//   });
-// });
