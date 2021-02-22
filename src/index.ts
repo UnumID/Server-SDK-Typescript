ecea250b9d8120ca637cf37891bebbc4a5d4950a
Ray Smets <rayjsmets@gmail.com>
2021-02-22 12:43:56 -0800

    [Types] exporting Proof & VerifiableCredential.

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { registerVerifier } from './verifier/registerVerifier';
 import { sendEmail } from './verifier/sendEmail';
 import { sendRequest } from './verifier/sendRequest';
 import { sendSms } from './verifier/sendSms';
-import { NoPresentation, Presentation, UnumDto, RegisteredVerifier, PresentationRequestResponse, RegisteredIssuer, VerifiedStatus } from './types';
+import { NoPresentation, Presentation, UnumDto, RegisteredVerifier, PresentationRequestResponse, RegisteredIssuer, VerifiedStatus, VerifiableCredential } from './types';
 import { verifyNoPresentation } from './verifier/verifyNoPresentation';
 import { verifyPresentation } from './verifier/verifyPresentation';
 import { verifyEncryptedPresentation } from './verifier/verifyEncryptedPresentation';
 import { registerIssuer } from './issuer/registerIssuer';
 import { issueCredential } from './issuer/issueCredentials';
 import { revokeCredential } from './issuer/revokeCredentials';
-import { CustError } from 'library-issuer-verifier-utility';
+import { CustError, Proof } from 'library-issuer-verifier-utility';
 
 export {
   // Issuer Functions
@@ -32,5 +32,8 @@ export {
   NoPresentation,
   Presentation,
   RegisteredIssuer,
-  CustError
+  VerifiableCredential,
+  // Util Types
+  CustError,
+  Proof
 };
