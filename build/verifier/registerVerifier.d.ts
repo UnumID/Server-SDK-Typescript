886c45502d0eccce30cbddda95440eab4c20a8ab
Ray Smets <rayjsmets@gmail.com>
2021-04-29 16:39:50 -0700

    [Build ] registerVerifier.

--- build/verifier/registerVerifier.d.ts
+++ build/verifier/registerVerifier.d.ts
@@ -1,10 +1,12 @@
 import { RegisteredVerifier, UnumDto } from '../types';
+import { VersionInfo } from '@unumid/types';
 /**
  * Handler for registering a Verifier with UnumID's SaaS.
  * @param name
  * @param customerUuid
  * @param url
  * @param apiKey
+ * @param versionInfo
  */
-export declare const registerVerifier: (name: string, customerUuid: string, url: string, apiKey: string) => Promise<UnumDto<RegisteredVerifier>>;
+export declare const registerVerifier: (name: string, customerUuid: string, url: string, apiKey: string, versionInfo?: VersionInfo[]) => Promise<UnumDto<RegisteredVerifier>>;
 //# sourceMappingURL=registerVerifier.d.ts.map
