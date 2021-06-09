91535aa235fd0617fcbc63c7a6669b5712862ebc
UnumID Admin <liam@unum.id>
2021-06-09 16:54:32 +0000

    [Release 3.0.2] Presentation presentationRequestUuid refactor to use the presentationRequestId for better versioning simplicity for the customer. Also updated the DecryptedPresentation type to only handle the presentation protobuf type.

--- build/verifier/verifyPresentation.js
+++ build/verifier/verifyPresentation.js
@@ -52,8 +52,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyPresentation = void 0;
 var types_1 = require("@unumid/types");
-// import { Presentation, EncryptedData, PresentationPb, PresentationRequestPb, ProofPb, UnsignedPresentationRequestPb, JSONObj, CredentialRequestPb } from '@unumid/types';
-// import { PresentationRequestDto, PresentationRequest, CredentialRequest } from '@unumid/types-v2';
 var requireAuth_1 = require("../requireAuth");
 var library_crypto_1 = require("@unumid/library-crypto");
 var logger_1 = __importDefault(require("../logger"));
