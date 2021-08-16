95c50b1fb8d29ac6f246823ddd558cf686966f0b
Ray Smets <rayjsmets@gmail.com>
2021-08-16 10:35:18 -0700

    [PresentationRequest] validation removing the omission of the version attribute.

--- src/verifier/verifyPresentation.ts
+++ src/verifier/verifyPresentation.ts
@@ -149,8 +149,8 @@ export const verifyPresentation = async (authorization: string, encryptedPresent
     // verify the presentation request signature if present
     if (presentationRequest && presentationRequest.presentationRequest) {
       // removing the version attribute that the saas adds
-      const presentationRequestWithoutVersion: PresentationRequest = omit(presentationRequest.presentationRequest, 'version');
-      const requestVerificationResult = await verifyPresentationRequest(authorization, presentationRequestWithoutVersion);
+      // const presentationRequestWithoutVersion: PresentationRequest = omit(presentationRequest.presentationRequest, 'version');
+      const requestVerificationResult = await verifyPresentationRequest(authorization, presentationRequest.presentationRequest);
       authorization = requestVerificationResult.authToken;
 
       // if invalid then can stop here but still send back the decrypted presentation with the verification results
