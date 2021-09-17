b3221121cd536377c7bd203f1102d2931d3bf79f
Ray Smets <rayjsmets@gmail.com>
2021-09-16 18:12:16 -0700

    [PresentationVerified] receipt handler, add default values to saas validation.

--- src/verifier/sendPresentationVerifiedReceipt.ts
+++ src/verifier/sendPresentationVerifiedReceipt.ts
@@ -16,7 +16,7 @@ import { handleAuthTokenHeader, makeNetworkRequest } from '../utils/networkReque
  * @param credentialTypes
  * @returns
  */
-export async function sendPresentationVerifiedReceipt (authorization: string, verifier: string, subject: string, reply: string, isVerified: boolean, reason?: string, issuers?: string[], credentialTypes?: string[]): Promise<string> {
+export async function sendPresentationVerifiedReceipt (authorization: string, verifier: string, subject: string, reply: string, isVerified: boolean, reason?: string, issuers: string[] = [], credentialTypes: string[] = []): Promise<string> {
   const receiptOptions = {
     type: 'PresentationVerified',
     verifier,
