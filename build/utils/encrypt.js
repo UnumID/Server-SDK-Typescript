b3a8c63bb34e735ff15a2a6d4e1fdb33f0be1579
Jacob Singer <jacobleesinger@gmail.com>
2021-11-05 14:52:19 -0700

    Rsa padding (#88)
    * [issueCredentials.test] fix types and disable inaccurate test
    
    * [package] update crypto lib to 1.7.0
    
    * [encrypt] pass publicKeyInfo rsaPadding to crypto library encrypt
    
    * [issueCredentials.test] add TODO comment on weird test

--- build/utils/encrypt.js
+++ build/utils/encrypt.js
@@ -14,7 +14,7 @@ var library_crypto_1 = require("@unumid/library-crypto");
  */
 exports.doEncrypt = function (did, publicKeyObj, data) {
     logger_1.default.debug('Performing encryption using public key', publicKeyObj);
-    var result = library_crypto_1.encrypt(did + '#' + publicKeyObj.id, publicKeyObj.publicKey, data, publicKeyObj.encoding);
+    var result = library_crypto_1.encrypt(did + '#' + publicKeyObj.id, publicKeyObj.publicKey, data, publicKeyObj.encoding, publicKeyObj.rsaPadding);
     logger_1.default.debug("Encrypted data result: " + result.data);
     return result;
 };
@@ -26,7 +26,7 @@ exports.doEncrypt = function (did, publicKeyObj, data) {
  */
 exports.doEncryptPb = function (did, publicKeyObj, data) {
     logger_1.default.debug('Performing byte array encryption using public key', publicKeyObj);
-    var result = library_crypto_1.encryptBytes(did + '#' + publicKeyObj.id, publicKeyObj.publicKey, data, publicKeyObj.encoding);
+    var result = library_crypto_1.encryptBytes(did + '#' + publicKeyObj.id, publicKeyObj.publicKey, data, publicKeyObj.encoding, publicKeyObj.rsaPadding);
     logger_1.default.debug("Encrypted data result: " + result.data);
     return result;
 };
