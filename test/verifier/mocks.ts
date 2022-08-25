cdf023952c896670b5054326b51d13337fff0338
Ray Smets <rayjsmets@gmail.com>
2022-08-25 16:23:24 -0700

    mocks

--- test/verifier/mocks.ts
+++ test/verifier/mocks.ts
@@ -201,7 +201,7 @@ export const makeDummyDidDocument = async (options: Partial<DidDocument> = {}): 
   if (!publicKey) {
     const keypairs = await createKeyPairSet();
 
-    publicKey: PublicKeyInfo = [
+    publicKey = [
       {
         id: getUUID(),
         publicKey: keypairs.signing.publicKey,
