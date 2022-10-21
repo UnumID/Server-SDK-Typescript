238af5b0a2627b455fa231ce4743673014a7f797
Ray Smets <rayjsmets@gmail.com>
2022-10-21 16:41:17 -0700

    [decrypt]

--- src/utils/decrypt.ts
+++ src/utils/decrypt.ts
@@ -1,6 +1,7 @@
 import { EncryptedData } from '@unumid/types';
 import logger from '../logger';
-import { decryptBytes, encryptBytes } from '@unumid/library-crypto';
+import { decryptBytes } from '@unumid/library-crypto';
+import { decryptBytes as decryptBytesBase58 } from '@unumid/library-crypto-v1';
 
 /**
  * Decrypt the data.
@@ -11,11 +12,23 @@ import { decryptBytes, encryptBytes } from '@unumid/library-crypto';
 export const doDecrypt = (privateRSAKey: string, data: EncryptedData): Buffer => {
   logger.debug('Performing byte array decryption');
 
-  const result = decryptBytes(
-    privateRSAKey,
-    data
-  );
+  try {
+    const result = decryptBytes(
+      privateRSAKey,
+      data
+    );
 
-  logger.debug('Decrypted data success.');
-  return result;
+    logger.debug('Decrypted data success.');
+    return result;
+  } catch (e) {
+    logger.debug(`Unable to decode using most recent crypto library with base64 encoding. Going to try with the v1, base58 crypto library. ${e}`);
+
+    const result = decryptBytesBase58(
+      privateRSAKey,
+      data
+    );
+
+    logger.debug('Decrypted data success.');
+    return result;
+  }
 };
